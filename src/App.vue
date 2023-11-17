bef1cab8e7cd51b5465f2d9f381fb97afd0a8e66
Yurii Rybachok <drummer2009@ukr.net>
2023-11-17 10:58:49 +0200

    add styling for project

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,49 @@
   <h1>Hello, World!</h1>
 </template>
 
-<style>
+<style lang="scss">
+.page {
+	background: #f1f5f9;
+	width: 100%;
+	min-height: 100vh;
+	padding-top: 2rem;
+}
 
+.container {
+	max-width: 600px;
+	margin: 0 auto;
+}
+
+h1 {
+	font-size: 48px;
+	margin-bottom: 2rem;
+	font-weight: 600;
+}
+
+.divider {
+	margin: 2rem 0;
+	border-color: #e2e8f0;
+}
+
+.task-container {
+	background: #ffffff;
+	padding: 1rem;
+
+	&.is-personal {
+		border-left: 3px solid green;
+	}
+
+	&.is-business {
+		border-left: 3px solid purple;
+	}
+}
+
+.task-wrapper {
+	display: flex;
+	align-items: center;
+}
+
+.vf-list-handle.task-sort-handle {
+	top: 1rem;
+}
 </style>
